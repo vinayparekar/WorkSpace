de505e024fe0635ed0dd92096d5c2cab38902b5e
vinay parekar <vinay.parekar@wework.com>
2020-02-20 16:09:43 -0800

    facebook phone - sparse matrix - completing transpose method

--- PrepFast/src/main/java/com/facebook/phone/SparseMatrix.java
+++ PrepFast/src/main/java/com/facebook/phone/SparseMatrix.java
@@ -5,8 +5,10 @@ import java.util.*;
 public class SparseMatrix {
 
     List<Map<Integer,Integer>> spaMatrix;
+    int rows;
+    int columns;
 
-    public SparseMatrix(List<List<Integer>> matrix){
+    SparseMatrix(List<List<Integer>> matrix){
 
         spaMatrix = new LinkedList<>();
         for(int i =0 ; i< matrix.size()  ;i++){
@@ -16,9 +18,27 @@ public class SparseMatrix {
             }
             spaMatrix.add(rowmap);
         }
+
+        rows = matrix.size();
+        columns = matrix.get(0).size();
     }
 
-    public void printSparseMatrix(){
+    SparseMatrix(List<Map<Integer,Integer>> matrix, int rows, int columns){
+
+        spaMatrix = new LinkedList<>();
+        for(int i =0 ; i< rows  ;i++){
+            Map<Integer,Integer> rowmap = new LinkedHashMap<>();
+            for(int j=0 ;j< columns ; j++){
+                rowmap.put(j, matrix.get(i).get(j));
+            }
+            spaMatrix.add(rowmap);
+        }
+
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    public static void printSparseMatrix(List<Map<Integer,Integer>> spaMatrix){
 
         for(Map map : spaMatrix){
             for(Object key : map.keySet()){
@@ -28,20 +48,38 @@ public class SparseMatrix {
         }
     }
 
+    public void createZeroSparseMatrix(List<Map<Integer,Integer>> zroSprMatrix, int rows, int columns){
+
+        for(int i =0 ; i< rows  ;i++){
+            Map<Integer,Integer> rowmap = new LinkedHashMap<>();
+            for(int j=0 ;j< columns ; j++){
+                rowmap.put(j, 0);
+            }
+            zroSprMatrix.add(rowmap);
+        }
+    }
+
     public List<Map<Integer,Integer>> getTransposeSparseMatrix(){
 
         List<Map<Integer,Integer>> trnSprMat = new LinkedList<>();
+        //create zero transpose matrix
+        createZeroSparseMatrix(trnSprMat, columns, rows);
         // transpose logic
+        for(int i=0 ; i< spaMatrix.size() ; i++){
+            for(Object key : spaMatrix.get(i).keySet()){
+                trnSprMat.get((Integer) key).put(i, spaMatrix.get(i).get((Integer) key));
+            }
+        }
         return  trnSprMat;
 
     }
 
-    public static  List<List<Integer>> generateRandomMatrix(int n, int m){
+    public static  List<List<Integer>> generateRandomMatrix(int rows, int columns){
 
         List<List<Integer>> matrix = new LinkedList<>();
-        for(int i=0 ; i< n ;i++){
+        for(int i=0 ; i< rows ;i++){
             List<Integer> rowList = new LinkedList<>();
-            for(int j=0 ; j<m ; j++){
+            for(int j=0 ; j<columns ; j++){
                 rowList.add((int) Math.round(Math.random()));
             }
             matrix.add(rowList);
@@ -61,12 +99,17 @@ public class SparseMatrix {
 
     public static void main(String[] args ){
 //        SparseMatrix spMat = new SparseMatrix()
-        List<List<Integer>> mat = generateRandomMatrix(5,5);
+        List<List<Integer>> mat = generateRandomMatrix(5,4);
         printMatrix(mat);
 
         System.out.print("\n +++++++++ creating sparse Matrix ++++++++++ \n\n");
 
         SparseMatrix sprMat = new SparseMatrix(mat);
-        sprMat.printSparseMatrix();
+        printSparseMatrix(sprMat.spaMatrix);
+
+        System.out.print("\n +++++++++ creating transpose of sparse Matrix ++++++++++ \n\n");
+        List<Map<Integer,Integer>> trnspMat =  sprMat.getTransposeSparseMatrix();
+        SparseMatrix trnspSprMat = new SparseMatrix(sprMat.getTransposeSparseMatrix(), trnspMat.size(), trnspMat.get(0).size() );
+        printSparseMatrix(trnspSprMat.spaMatrix);
     }
 }
